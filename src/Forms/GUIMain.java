a17c4c30a937f806cd1f5e0e28b5933a03463c30
nhc.hcmuns <nhc.hcmuns@2626b8c2-1318-11de-99c7-9d618077940f>
2009-04-03 18:08:18 +0000

    Up lại mấy cái Form, Form này là Generate nhưng ko vừa ý lắm, chắc là phải tự "Do It By Yourself" cái Form luôn quá

---
+++ src/Forms/GUIMain.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Forms;
+import javax.swing.*;
+/**
+ *
+ * @author Spazee
+ */
+public class GUIMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       frmMove form = new frmMove();
+       form.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       form.setVisible(true);
+    }
+
+}
